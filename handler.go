972197f77fd1cff0cf7a4ff99e08019d394cab02
Nishant <palln6935@gmail.com>
2025-09-18 09:33:14 +0530

    init handlers

--- handler.go
+++ handler.go
@@ -1,9 +1,22 @@
 package main
 
+import "sync"
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": pong,
+	"GET":  get,
+	"SET":  set,
 }
 
 func pong([]Value) Value {
 	return Value{typ: "string", str: "PONG"}
+}
+
+func get([]Value) Value {
+	mu := sync.Mutex
+
+}
+func set([]Value) Value {
+	mu := sync.Mutex
+
 }
